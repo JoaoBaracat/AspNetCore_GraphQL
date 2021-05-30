976d530d8b43d89752b7543cd3af742bc746f930
baracat <joaodobingo@gmail.com>
2021-05-30 16:48:22 -0300

    feat: added new GraphQL project and course implementing Query, Mutation and Subscription. Also used scalar types, dataloader, websocket (for the subscription) and created a Web UI

---
+++ building-graphql-apis-aspdotnet-core/04/demos/CarvedRock.Web/Clients/ProductHttpClient.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using CarvedRock.Web.Models;
+using Newtonsoft.Json;
+
+namespace CarvedRock.Web.HttpClients
+{
+    public class ProductHttpClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductHttpClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<Response<ProductsContainer>> GetProducts()
+        {
+            var response = await _httpClient.GetAsync(@"?query= 
+            { products 
+                { id name price rating photoFileName } 
+            }");
+
+            var responseAleases = await _httpClient.GetAsync(@"?query= 
+            { results: products 
+                { id name price rating photo: photoFileName } 
+            }");    // adding the "photo:" and "results:" before the item it sets an alias
+
+
+            var stringResult = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Response<ProductsContainer>>(stringResult);
+        }
+    }
+}
